880b53b71604e10576a5ec5a113bc19d72d6fae5
nguyenluat19 <128703641+nguyenluat19@users.noreply.github.com>
2025-10-01 22:03:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use("/api/v4/", reviewRouter);
 //API thanh toán
 app.use("/api/v5/", paymentRouter);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   startMBBankJob(app);
